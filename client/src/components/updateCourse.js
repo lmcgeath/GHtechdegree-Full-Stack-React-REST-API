7a214f983eb59aa1ca6aa806842cc43203743b19
lmcgeath <49741190+lmcgeath@users.noreply.github.com>
2020-02-07 09:45:09 -0800

    Rename updateCourse.js to UpdateCourse.js

--- client/src/components/updateCourse.js
+++
@@ -1,1 +0,0 @@
-//- UpdateCourse - This component provides the "Update Course" screen by rendering a form that allows a user to update one of their existing courses. The component also renders an "Update Course" button that when clicked sends a PUT request to the REST API's /api/courses/:id route. This component also renders a "Cancel" button that returns the user to the "Course Detail" screen.
