3705d9fdfb6e9c217c1b61d36725450538bcbe32
Lucas McGeath <49741190+lmcgeath@users.noreply.github.com>
2020-03-02 16:09:36 -0800

    index.js database connect

--- api/seed/index.js
+++ api/seed/index.js
@@ -1,3 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
 'use strict';
 
 const Sequelize = require('sequelize');
