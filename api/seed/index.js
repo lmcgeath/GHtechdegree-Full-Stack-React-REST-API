858ab7b0747c5430deca28dd42146416f91ea269
Lucas McGeath <49741190+lmcgeath@users.noreply.github.com>
2020-03-02 20:45:42 -0800

    Heroku configs

--- api/seed/index.js
+++ api/seed/index.js
@@ -1,11 +1,21 @@
-const { Pool } = require('pg');
-const pool = new Pool({
+'use strict';
+
+const { Client } = require('pg');
+
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: true,
 });
 
+client.connect();
 
-'use strict';
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const Sequelize = require('sequelize');
 const promiseFinally = require('promise.prototype.finally');
