031fcff9b83d70f53c4db90901f141a23bce3f68
Lucas McGeath <49741190+lmcgeath@users.noreply.github.com>
2020-03-03 09:54:39 -0800

    Adding `build` to source control

---
+++ client/build/precache-manifest.90846a94daa18aa7025944812be209af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc18bf50255422aa0bf26b39d49f2a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3647f31d14b838fdd4a2",
+    "url": "/static/css/main.1c6fd386.chunk.css"
+  },
+  {
+    "revision": "831593984f8a58ab2914",
+    "url": "/static/js/2.0225eefc.chunk.js"
+  },
+  {
+    "revision": "3647f31d14b838fdd4a2",
+    "url": "/static/js/main.b1d1a086.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
